a9341d957ec80bf8199f1f449a2d55bed681a209
ashaid <ashaid2@lsu.edu>
2022-02-16 08:42:28 +0000

    initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+
+// @ts-ignore
+import Home from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        {/* // <Route index element={<Home />}></Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import logo from "../logo.svg";
+
+export interface IHomeProps {}
+
+export interface IHomeState {}
+
+export default class Home extends React.Component<IHomeProps, IHomeState> {
+  constructor(props: IHomeProps) {
+    super(props);
+
+    this.state = {};
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
