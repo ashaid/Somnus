c599cb9585ad71700fcdd0563636627672ea1735
ashaid <ashaid2@lsu.edu>
2022-04-13 08:20:30 +0000

    added eye thing

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -4,19 +4,35 @@ import { Box } from "@mui/system/";
 import Typography from "@mui/material/Typography";
 import { useParallax } from "react-scroll-parallax";
 
+const openEye = require("../media/chars/open.png");
+const closingEye = require("../media/chars/closing.png");
+const squintingEye = require("../media/chars/squinting.png");
+const closedEye = require("../media/chars/closed.png");
+const eyeImages = { openEye, closingEye, squintingEye, closedEye };
 export interface IInfoProps {
   progress?: number;
+  imageSrc?: string;
 }
 
 export function Info(props: IInfoProps) {
   const [progress, setProgress] = React.useState<number>(0);
+  const [imageSrc, setImageSrc] = React.useState(eyeImages.openEye);
   const { ref } = useParallax<HTMLDivElement>({
-    speed: 25,
+    speed: 5,
     onProgressChange: (progress) => setProgress(progress),
   });
 
   useEffect(() => {
     console.log(progress);
+    if (progress < 0.5) {
+      setImageSrc(eyeImages.openEye);
+    } else if (progress > 0.5 && progress < 0.7) {
+      setImageSrc(eyeImages.closingEye);
+    } else if (progress > 0.7 && progress < 0.87) {
+      setImageSrc(eyeImages.squintingEye);
+    } else {
+      setImageSrc(eyeImages.closedEye);
+    }
   });
 
   return (
@@ -35,11 +51,30 @@ export function Info(props: IInfoProps) {
           component="img"
           sx={{
             position: "absolute",
-            width: "25vh",
+            width: "50vh",
+            left: "30vw",
           }}
-          src={require("../media/chars/open.png")}
+          src={imageSrc}
+          key={imageSrc}
         />
       </div>
+      <Box>
+        <Typography
+          variant="h1"
+          fontWeight="bold"
+          fontFamily="roboto"
+          component="div"
+          color="white"
+          sx={{
+            textAlign: "center",
+            justifyContent: "center",
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          Demo Text
+        </Typography>
+      </Box>
     </Box>
   );
 }
