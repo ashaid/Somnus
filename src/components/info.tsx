ceee91578968770c679364f61efec0330e71c956
tony <yesl@DESKTOP-6HMH74M.localdomain>
2022-05-03 23:43:30 -0500

    more changes to headshots

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -5,21 +5,33 @@ import Typography from "@mui/material/Typography";
 import { useParallax } from "react-scroll-parallax";
 import Screenshot from "../media/images/screenshot.png";
 import Screenshot2 from "../media/images/screenshot2.png";
-import myProPic from "../media/headshots/myProPic.jpg"
+import tony from "../media/headshots/tony-hs.jpg";
+
+//@ts-ignore
+import CrossfadeImage from "react-crossfade-image"
 
 const openEye = require("../media/chars/open.png");
 const closingEye = require("../media/chars/closing.png");
 const squintingEye = require("../media/chars/squinting.png");
 const closedEye = require("../media/chars/closed.png");
 const eyeImages = { openEye, closingEye, squintingEye, closedEye };
+
+const greenForest = require("../media/images/greenForest.png");
+const DreamForest = require("../media/images/DreamForest.png")
+const forestImages = [ greenForest, DreamForest]
 export interface IInfoProps {
   progress?: number;
   imageSrc?: string;
+  blurAmount?: number;
+  imageIndex?: number;
 }
 
 export function Info(props: IInfoProps) {
   const [progress, setProgress] = React.useState<number>(0);
   const [imageSrc, setImageSrc] = React.useState(eyeImages.openEye);
+  const [blurAmount, setBlurAmount] = React.useState<number>(0);
+  const [imageIndex, setImageIndex] = React.useState<number>(0);
+  
   const { ref } = useParallax<HTMLDivElement>({
     speed: 20,
     onProgressChange: (progress) => setProgress(progress),
@@ -29,22 +41,37 @@ export function Info(props: IInfoProps) {
 
   useEffect(() => {
     console.log(progress);
+    console.log(blurAmount)
+    console.log(imageIndex)
     if (progress < 0.5) {
       setImageSrc(eyeImages.openEye);
+      setBlurAmount(10);
+      // setImageIndex(0);
     } else if (progress > 0.5 && progress < 0.7) {
       setImageSrc(eyeImages.closingEye);
     } else if (progress > 0.7 && progress < 0.87) {
       setImageSrc(eyeImages.squintingEye);
-    } else {
+      setImageIndex(0);
+    } else if (progress > 0.87) {
       setImageSrc(eyeImages.closedEye);
+      
+    } 
+    if (progress > .92 && progress < .94) {
+      setBlurAmount(4);
+      setImageIndex(1);
+    } else if (progress > 0.94 && progress < 0.96) {
+      setBlurAmount(3);
+      
+    } else if (progress > 0.96) {
+      setBlurAmount(0);
     }
+
   });
 
   return (
     <Box
       sx={{
-        margin: "5%",
-        height: "150vh",
+        margin: "15%",
       }}
       ref={ref}
     >
@@ -118,21 +145,15 @@ export function Info(props: IInfoProps) {
             </Typography>
           </Typography>
         </Box>
-        <Box className="column center">
-          <Box
-            component="img"
-            sx={{
-              width: "40vh",
-              position: "absolute",
-            }}
-            src={Screenshot}
-            // key={imageSrc}
-          />
+        <Box className="column center" sx={{}}>
+          <CrossfadeImage src={forestImages[imageIndex]} style={{height: "200px", borderRadius: "20%", flexGrow: 0}} duration={1000} />
         </Box>
       </Box>
-      <Box className="row 4" sx={{ height: "20%" }} />
-      <Box className="row 5">
-        <Box className="column center">
+      {/* <Box className="row 4" sx={{ height: "20%" }} /> */}
+
+      <Box className="row 5" sx={{ }}>
+
+        <Box className="column">
           <Typography
             variant="h1"
             fontWeight="bold"
@@ -140,23 +161,74 @@ export function Info(props: IInfoProps) {
             component="div"
             color="white"
           >
-            About NITE TIME Studios
+           NITE TIME Studio
           </Typography>
 
         </Box>
-        <Box className="row 6 center" sx={{height: "20%vh"}}>     
-        <Box className="column"><Box component="img" sx={{padding: 5}}  src={myProPic} />  <Typography
-            variant="h3"
-            fontFamily="roboto"
-            component="div"
-            color="white"
-          >
-            Joe Biden
-          </Typography></Box>     
-        <Box className="column"><Box component="img" sx={{padding: 5}} src={myProPic} /> Joe Biden</Box>   
-        <Box className="column"><Box component="img" sx={{padding: 5}} src={myProPic} /> Joe Biden</Box>   
-        <Box className="column"><Box component="img" sx={{padding: 5}} src={myProPic} /> Joe Biden</Box>   
-          </Box>
+
+        <Box className="row 6" sx={{height: "20%vh", alignItems: "flex-start", filter: `blur(${blurAmount}px)` }}> 
+        
+          <Box className="column" > 
+            <Box component="img" sx={{padding: 5, maxWidth: "100%"}}  src={tony} /> 
+              <Typography
+                  variant="h3"
+                  fontFamily="roboto"
+                  component="div"
+                  color="white"
+                  sx={{}}
+                >
+                  Joe Biden
+                </Typography>
+          </Box> 
+          <Box className="column"> 
+            <Box component="img" sx={{padding: 5, maxWidth: "100%"}}  src={tony} /> 
+              <Typography
+                  variant="h3"
+                  fontFamily="roboto"
+                  component="div"
+                  color="white"
+                  sx={{}}
+                >
+                  Joe Biden
+                </Typography>
+          </Box> 
+          <Box className="column"> 
+            <Box component="img" sx={{padding: 5, maxWidth: "100%"}}  src={tony} /> 
+              <Typography
+                  variant="h3"
+                  fontFamily="roboto"
+                  component="div"
+                  color="white"
+                  sx={{}}
+                >
+                  Joe Biden
+                </Typography>
+          </Box> 
+          <Box className="column"> 
+            <Box component="img" sx={{padding: 5, maxWidth: "100%"}}  src={tony} /> 
+              <Typography
+                  variant="h3"
+                  fontFamily="roboto"
+                  component="div"
+                  color="white"
+                  sx={{}}
+                >
+                  Joe Biden
+                </Typography>
+          </Box> 
+          <Box className="column"> 
+            <Box component="img" sx={{padding: 5, maxWidth: "100%"}}  src={tony} /> 
+              <Typography
+                  variant="h3"
+                  fontFamily="roboto"
+                  component="div"
+                  color="white"
+                  sx={{}}
+                >
+                  Joe Biden
+                </Typography>
+          </Box> 
+        </Box>     
       </Box>
     </Box>
   );
